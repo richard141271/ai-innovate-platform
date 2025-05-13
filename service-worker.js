8abe4b527197421937d4c4e5920bfd8f596c8f78
richard141271 <68450987+richard141271@users.noreply.github.com>
2025-05-14 01:47:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('aiinnovate-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/manifest.json'
-      ]);
-    })
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+// V2 service worker
